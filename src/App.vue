28e55ba1edc7a42664f9f1f7a285ad7c8f711855
Matjaž Cvek <matjaz.cvek@gmail.com>
2024-12-09 14:40:42 +0100

    add gallery path, view and layout component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
+@import "@/styles/_variables.scss";
+
 #app {
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $text-color;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  margin: 0;
 }
 </style>
---
+++ src/views/GalleryView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="galleryView">
+    <img class="logo" alt="logo" src="../assets/logo.svg" />
+    <GalleryLayout></GalleryLayout>
+  </div>
+</template>
+
+<script lang="ts">
+import GalleryLayout from "@/components/GalleryLayout.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "GalleryView",
+  components: {
+    GalleryLayout,
+  },
+});
+</script>
+
+<style lang="scss">
+@import "@/styles/_variables.scss";
+
+.galleryView {
+  height: 100vh;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  background-color: $primary-color;
+
+  .logo {
+    margin: 10px;
+    width: 94px;
+    height: 14px;
+    margin-bottom: 10px;
+  }
+}
+</style>
