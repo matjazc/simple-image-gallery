65a19b3a9de0526d9ea5a37e1cd29ebe3c0d719d
Matjaž Cvek <matjaz.cvek@gmail.com>
2024-12-11 16:06:03 +0100

    fix overflow issues

--- src/components/ImgDetailsContent.vue
+++ src/components/ImgDetailsContent.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="imgDetailsContent">
-    <div class="imgSize">
-      {{ isLoading ? "..." : `${imgDetails?.width}x${imgDetails?.height}` }}
-    </div>
-    <div v-if="imgDetails" class="container">
-      <img :src="imgDetails.download_url" />
+  <div class="wrapper">
+    <div class="imgDetailsContent">
+      <div class="imgSize">
+        {{ isLoading ? "..." : `${imgDetails?.width}x${imgDetails?.height}` }}
+      </div>
+      <div v-if="imgDetails" class="container">
+        <img :src="imgDetails.download_url" />
+      </div>
     </div>
   </div>
 </template>
@@ -30,28 +32,35 @@ export default {
 <style lang="scss">
 @import "@/styles/_variables.scss";
 
-.imgDetailsContent {
+.wrapper {
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-  margin: 20px 20px 0;
-  border-radius: 8px 8px 0 0;
-  background-color: $primary-color;
+  overflow: auto;
 
-  .imgSize {
-    font-size: 14px;
-    font-weight: bold;
-    color: $secondary-text-color;
-    margin: 15px;
-  }
-
-  .container {
+  .imgDetailsContent {
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    flex-grow: 1;
+    margin: 20px 20px 0;
+    border-radius: 8px 8px 0 0;
+    background-color: $primary-color;
 
-    img {
-      max-width: 80%;
-      border-radius: 8px;
+    .imgSize {
+      font-size: 14px;
+      font-weight: bold;
+      color: $secondary-text-color;
+      margin: 15px;
+    }
+
+    .container {
+      display: flex;
+      justify-content: center;
+
+      img {
+        max-width: 80%;
+        border-radius: 8px;
+      }
     }
   }
 }
